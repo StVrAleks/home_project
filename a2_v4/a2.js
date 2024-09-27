f8a1d1bdb22f18d69f3e5dfbebb0565dca4b3901
VeraS. <orchid86@yandex.ru>
2024-09-27 14:46:17 +0300

    dop proverki

---
+++ a2_v4/a2.js
@@ -0,0 +1,41 @@
+var myStroka = prompt('Введите строку');
+var newstroka = funcStroka(myStroka);
+alert("_" + newstroka + "_");
+
+
+  
+function funcStroka(myItem){
+    var lStroka = myItem.length;
+    for(var i = 0; lStroka; i++)
+    {
+        var indexF = 0;
+        if(myItem[i] !== " ")
+            {
+            indexF = i;           
+            break; 
+            }
+        if(indexF === 0 && i === lStroka-1)
+            {
+            console.log("Символов в строке: " + lStroka);    
+            return "";    
+            }    
+    }  
+    
+    for(var i = lStroka-1; i > indexF; i = i - 1)
+        {
+            var indexS = lStroka;          
+            if(myItem[i] !== " ")
+                {
+                indexS = i+1;
+                break; 
+                }
+        }  
+    if(indexF === 0 && indexS === lStroka)
+        {
+            console.log("Строка без внешних пробелов. Количество символов: " + lStroka);
+            return myItem;   
+        }   
+
+        var myNewItem = myItem.substring(indexF, indexS);  
+   return myNewItem;
+}
