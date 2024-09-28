c3c85574256d69ab1b4d92ed0be7072c8077feb1
VeraS. <orchid86@yandex.ru>
2024-09-28 09:36:31 +0300

    update

--- arhive/a2/a2.js
+++ arhive/a2/a2.js
@@ -1,29 +1,41 @@
 var myStroka = prompt('Введите строку');
 var newstroka = funcStroka(myStroka);
 alert("_" + newstroka + "_");
-
-
   
 function funcStroka(myItem){
+    var flag = 0;
     var lStroka = myItem.length;
-    for(var i = 0; lStroka; i++)
+    for(var i = 0; i < lStroka; i++)
     {
-        var indexF = 0;
-        if(myItem[i] !== " "){
-            indexF = i;            
+        var indexF = 0;      
+        if(myItem[i] !== " ")
+            {
+            indexF = i; 
+            flag = 'not';  
             break; 
-        }
+            }
+        flag = i;            
     }  
+    if(flag === lStroka - 1)
+       {
+       console.log("Символов в строке: " + lStroka);    
+       return "";    
+       }    
+    
     for(var i = lStroka-1; i > indexF; i = i - 1)
         {
-            var indexS = myItem.length;          
+            var indexS = lStroka;          
             if(myItem[i] !== " ")
                 {
-                indexS = i;
+                indexS = i+1;
                 break; 
                 }
         }  
-        var myNewItem = '';
-        myNewItem = myItem.substring(indexF, indexS);  
+    if(indexF === 0 && indexS === lStroka)
+        {
+            console.log("Строка без внешних пробелов. Количество символов: " + lStroka);
+            return myItem;   
+        }   
+        var myNewItem = myItem.substring(indexF, indexS);  
    return myNewItem;
 }
