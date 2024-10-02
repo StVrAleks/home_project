831697d5ee5d447cebd9412d2344fa368ce8dfb1
VeraS. <orchid86@yandex.ru>
2024-10-02 15:54:41 +0300

    a3 v2

--- a3/palindrom.js
+++ a3/palindrom.js
@@ -1,20 +1,22 @@
 var myStroka = prompt("Введите строку для анализа?");
 
-myStroka = myStroka.toLowerCase();
-myStroka = myStroka.replace(/\s/g, '');
-myStroka = myStroka.replace(/[.,\/#!$%\^&\*;:"{}=\-_`~()?№'+]/g, '');
-myStroka = myStroka.replace('ъ', 'ь', '');
-myStroka = myStroka.replace('ё', 'е');
-alert(palindrom(myStroka));
+if(palindrom(myStroka) === true)
+    alert('это палиндром');
+else
+    alert('это не палиндром');
 
 function palindrom(NewStr)
 {
+NewStr = NewStr.toLowerCase();
+NewStr = NewStr.replace(/[\s.,\/#!$%\^&\*;:"{}=\-_`~()?№'+ъь]/g, '');
+NewStr = NewStr.replace('ё', 'е');
+
 var allLenght = NewStr.length;    
 var polLenght = Math.floor(0.5 * NewStr.length);
 for(var i = 0; i <= polLenght; i++)
     {
         if(NewStr[i] !== NewStr[allLenght - i - 1])
-            return 'это не палиндром';
+            return false;
     }
-return 'это палиндром';
+return true;
 }
