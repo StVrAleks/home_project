8556e7798289785123964a63b7b85e1f7f965402
VeraS. <orchid86@yandex.ru>
2024-10-18 16:01:53 +0300

    v.2

--- hw10/color.js
+++ hw10/color.js
@@ -12,18 +12,21 @@ function randomDiap(n,m) {
 function mood(colorsCount) {
 if(colorsCount > 8)
     return console.log("Введите количество цветов менее 9.");
-  
-const colors = { 1:'', 2:'красный', 3:'оранжевый', 4:'жёлтый', 5:'зелёный', 6:'голубой', 7:'синий', 8:'фиолетовый' };
+if(colorsCount < 0)
+    return console.log("Введите положительное число");  
+const colors = ['', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый' ];
 var n;
-const colorName = {[n]:'' };
+const colorName = [];
 
 console.log( 'цветов: ' + colorsCount );
+var flag;
 for ( let i=1; i<=colorsCount; i++ ) {
-   do{
-      n=randomDiap(1,7);        
-     } while(n in colorName);
+  do{
+      n=randomDiap(1,7);
+      flag = colorName.some(item => item === colors[n])        
+    } while(flag);
       colorName[n] = colors[n];
-      console.log( colorName[n] );
+      console.log( colorName[n]);
 }
 }
 
