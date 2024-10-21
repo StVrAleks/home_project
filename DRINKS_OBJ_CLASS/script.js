b7c669afc7c6a321ab9d0a17cfbed0a9eae95cb9
VeraS. <orchid86@yandex.ru>
2024-10-21 14:55:39 +0300

    last mod 21/10

--- DRINKS_OBJ_CLASS/script.js
+++ DRINKS_OBJ_CLASS/script.js
@@ -1,29 +1,32 @@
-function ObjStorageFunc(){
-    
-    const self = this;
-    this.drinkInfo ={};
-    //сохраняет указанное значение под указанным ключом; если под этим ключом уже сохранено какое-то значение — оно должно быть перезаписано; 
-    self.addValue = function(key, keyVal) 
-        {
-          this.drinkInfo[key] =  keyVal;
-          console.log(key, this.drinkInfo);
-        }
+class ObjStorageFunc{
+    //сохраняет указанное значение под указанным ключом; если под этим ключом уже сохранено какое-то значение — оно должно быть перезаписано;
+    constructor(){
+      this.drinkInfo ={};
+    }
+
+    addValue = function(key, keyVal) 
+     {
+      this.drinkInfo[key] =  keyVal;
+      console.log(key, this.drinkInfo);
+     }
     //возвращает значение по указанному ключу либо undefined;
-    self.getValue = function(key) {return this.drinkInfo[key];}        
+    getValue = function(key) {return this.drinkInfo[key];}
+ 
     //удаляет значение с указанным ключом, возвращает true если значение было удалено и false если такого значения не было в хранилище;
-    self.deleteValue = function(key) {
-                if(key in this.drinkInfo)
-                    {
-                      delete this.drinkInfo[key];
-                      return true;
-                    } 
-                    return false;
-        }   
+    deleteValue = function(key) {
+            if(key in this.drinkInfo)
+                {
+                  delete this.drinkInfo[key];
+                  return true;
+                } 
+                return false;
+    }
     //возвращает массив, состоящий из одних ключей. Класс должен быть чистым (не должен использовать никаких глобальных переменных, не должен «пачкать экран»).
-    self.getKeys = function() {return Object.keys(this.drinkInfo);}        
+    getKeys = function() {return Object.keys(this.drinkInfo);}
 }
 
-var drinkStorage = new ObjStorageFunc();
+let  drinkStorage = {};
+drinkStorage = new ObjStorageFunc();
 
 function buttonForAdd()
 {  
@@ -43,7 +46,8 @@ function buttonForGet()
       alert(`
  Название напитка: ${foundName}
  Алкогольность: ${infoLine['алкогольный:']}
- Рецепт: \n ${infoLine['рецепт приготовления:']}`);
+ Рецепт: \n ${infoLine['рецепт приготовления:']}
+            `);
    }
    else{
        alert('Такого напитка нет.');
@@ -62,5 +66,6 @@ function buttonForGetKeys()
 {
   var infoLine = drinkStorage.getKeys();
   alert(`
-Перечень всех напитков:\n ${infoLine}`);
+Перечень введенных напитков:
+${infoLine}`);
 }
