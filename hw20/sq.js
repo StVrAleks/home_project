d0709b99291493d65dcb7c65f0a0b7a6c3deae09
VeraS. <orchid86@yandex.ru>
2024-10-23 16:11:57 +0300

    много вопросов

--- hw20/sq.js
+++ hw20/sq.js
@@ -11,7 +11,10 @@
 
         if ( d==0 )
             return [ -b/(2*a) ]; // один корень
-
+/*Вариант 1*/
+/*     if (a == 0)
+         return ['Исходные параметры введены неверно. а не может быть равным нулю' ];*/
+//
         const x1=(-b+Math.sqrt(d))/(2*a);
         const x2=(-b-Math.sqrt(d))/(2*a);
         return [ x1, x2 ]; // два корня
@@ -22,45 +25,50 @@
         {
             console.log('тест 1,1,1 -> нет корней');
             const roots=squareRoots(1,1,1);
-            console.log( (roots.length==0)
+            console.log(roots, (roots.length==0)
                 ?'пройден':'НЕ ПРОЙДЕН!' )
         }
 
         {
             console.log('тест 1,-2,-3 -> два корня 3,-1');
             const roots=squareRoots(1,-2,-3);
-            console.log( ((roots.length==2)&&(roots[0]==3)&&(roots[1]==-1))
+            console.log(roots, ((roots.length==2)&&(roots[0]==3)&&(roots[1]==-1))
                 ?'пройден':'НЕ ПРОЙДЕН!' )
         }
 
         {
             console.log('тест -1,-2,15 -> два корня -5,3');
             const roots=squareRoots(-1,-2,15);
-            console.log( ((roots.length==2)&&(roots[0]==-5)&&(roots[1]==3))
+            console.log(roots, ((roots.length==2)&&(roots[0]==-5)&&(roots[1]==3))
                 ?'пройден':'НЕ ПРОЙДЕН!' )
         }
 
         {
             console.log('тест 1,12,36 -> один корень -6');
             const roots=squareRoots(1,12,36);
-            console.log( ((roots.length==1)&&(roots[0]==-6))
+            console.log(roots, ((roots.length==1)&&(roots[0]==-6))
                 ?'пройден':'НЕ ПРОЙДЕН!' )
         }
 
         {
-            console.log('тест 0,5,-10 -> один корень 2');
+            //console.log('тест 0,5,-10 -> один корень 2');
+            console.log('тест 0,5,-10 -> а не может быть равым 0');
             const roots=squareRoots(0,5,-10);
-            console.log( ((roots.length==1)&&(roots[0]==2))
+            //Вариант 2
+            console.log(roots, ((roots.length==2))
+            //console.log(roots, ((roots.length==1)&&(roots[0]==2))
                 ?'пройден':'НЕ ПРОЙДЕН!' )
         }
 
     }
 
     function ttt() {
-        const a=Number(prompt('Введите a'));
+ 
+        const a = Number(prompt('Введите a > 0'));
         const b=Number(prompt('Введите b'));
         const c=Number(prompt('Введите c'));
         const roots=squareRoots(a,b,c);
+
 
         if ( !roots.length )
             alert('корней нет!');
