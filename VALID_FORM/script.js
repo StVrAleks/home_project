b380cbe2f180831f38878090d6429a442a89601c
Vera Sotniokva <orchid86@yandex.ru>
2024-10-30 21:12:33 +0300

    2

--- VALID_FORM/script.js
+++ VALID_FORM/script.js
@@ -4,7 +4,7 @@ const formDef1=
 {label:'URL сайта:',kind:'longtext',name:'siteurl'},
 {label:'Посетителей в сутки:',kind:'number',name:'visitors'},
 {label:'E-mail для связи:',kind:'shorttext',name:'email'},
-{label:'Рубрика каталога:',kind:'dropdown',name:'division',
+{label:'Рубрика каталога:',kind: 'dropdown',name:'division',
 variants:[{text:'здоровье',value:1},
           {text:'домашний уют',value:2},
           {text:'бытовая техника',value:3}]},
@@ -26,24 +26,23 @@ const formDef2=
 {caption:'Зарегистрироваться',kind:'submit'},
 ];
 const form1 = document.getElementById('formI');
-form1.style.width = "60%";
-form1.style.borderTop = '1px solid grey';
-form1.style.borderBottom = '1px solid grey';
-//var form2 = document.getElementById('formII');
+const form2 = document.getElementById('formII');
 const addNewFORM = addForm(form1, formDef1);
-//var addNewFORM = addForm(form2, formDef2);
+const addNewFORM2 = addForm(form2, formDef2);
 
 function addForm(teg, formСontent){
     var objKinds = {'longtext':'text', 
                     'number':'number', 
                     'shorttext':'text', 
+                    'dropdown':'select',    
                     'radio':'radio', 
                     'check':'checkbox', 
                     'memo':'textarea',
                     'submit':'submit'};
-   // console.log(teg);
     var inputElement = [];  
     var labelElement = [];  
+    var selectElement = [];  
+    var labelForElement = [];
     var divElement = document.createElement("div");
     for(var i = 0; i < formСontent.length; i++)
         {
@@ -52,15 +51,63 @@ function addForm(teg, formСontent){
             labelElement[i].innerText = formСontent[i].label || formСontent[i].text;
         if(formСontent[i].text)
             labelElement[i].for = formСontent[i].text.length;
-            
-            divElement.appendChild(labelElement[i]);  
         
         if(formСontent[i].kind === 'memo')
-            inputElement[i] = document.createElement("textarea");    
+        {
+            inputElement[i] = document.createElement("textarea"); 
+            inputElement[i].type = objKinds[formСontent[i].kind];
+            inputElement[i].name = formСontent[i].name;
+            labelElement[i].appendChild(inputElement[i]);
+            divElement.appendChild(labelElement[i]);  
+        }
+        else if(formСontent[i].kind === 'dropdown')  
+        {
+            inputElement[i] = document.createElement("select");
+            inputElement[i].type = objKinds[formСontent[i].kind];  
+            inputElement[i].name = formСontent[i].name;
+            var itemVar=formСontent[i].variants;
+            for(j=0; j<itemVar.length; j++)
+            {
+                selectElement[j] = document.createElement("option");
+                selectElement[j].value = itemVar[j].value;
+                selectElement[j].innerText = itemVar[j].text;
+                inputElement[i].appendChild(selectElement[j]);    
+            }
+            labelElement[i].appendChild(inputElement[i]);
+            divElement.appendChild(labelElement[i]);  
+        }    
+        else if(formСontent[i].kind === 'radio')  
+            {
+                var itemVar=formСontent[i].variants;
+                for(j=0; j<itemVar.length; j++)
+                {
+                    labelForElement[j] = document.createElement("label");              
+                    labelForElement[j].innerText = itemVar[j].text;  
+                    labelElement[i].appendChild(labelForElement[j]);                     
+                    selectElement[j] = document.createElement("input");
+                    selectElement[j].name = formСontent[i].name;
+                    selectElement[j].type = objKinds[formСontent[i].kind];
+                    selectElement[j].value = itemVar[j].value;
+                //    selectElement[j].id = j;
+                    selectElement[j].innerText = itemVar[j].text;
+                    labelForElement[j].appendChild(selectElement[j]); 
+       
+                //    labelForElement[j].for = selectElement[j].id;
+                }
+                divElement.appendChild(labelElement[i]);  
+            }  
         else
+        {
             inputElement[i] = document.createElement("input");
-        inputElement[i].id = formСontent[i].text.length;
-        inputElement[i].type = objKinds[formСontent[i].kind];   
+            inputElement[i].type = objKinds[formСontent[i].kind];
+            inputElement[i].name = formСontent[i].name;
+            if(formСontent[i].caption)
+                inputElement[i].value = formСontent[i].caption;
+            labelElement[i].appendChild(inputElement[i]);
+            divElement.appendChild(labelElement[i]);              
+        }}
+     //   $input(type='checkbox').atr('checked','checked');
+   /*     inputElement[i].type = objKinds[formСontent[i].kind];
         
         objKinds[formСontent[i].kind]
         
@@ -72,15 +119,18 @@ function addForm(teg, formСontent){
         
         if(formСontent[i].value)
             inputElement[i].value = formСontent[i].value;
+        if(formСontent[i].text)
+            inputElement[i].innerText = formСontent[i].text;
     //    console.log(formСontent[i].variants);
 
         if(formСontent[i].variants)
             var addItem = addForm(inputElement[i], formСontent[i].variants);
-        divElement.appendChild(inputElement[i]);                 
-        }
-        teg.appendChild(divElement);  
+        labelElement[i].appendChild(inputElement[i]);
+        divElement.appendChild(labelElement[i]);                 
+        }*/
+    //    teg.appendChild(divElement);  
   //      console.log(divElement);        
-return true;
+return teg.appendChild(divElement);
 } 
 
 
@@ -90,4 +140,4 @@ return true;
         inputEnement[i].setAttribute('caption', formСontent[i].caption);
         inputEnement[i].setAttribute('display', 'block');*/       
         // labelElement[i].style.padding = '5px';
-       // labelElement[i].style.display = 'block';
+       // labelElement[i].style.display = 'block';*/
