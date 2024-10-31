32ebe34ae01eb9702c173c9755cc6da33c3eed15
VeraS. <orchid86@yandex.ru>
2024-10-31 08:34:07 +0300

    full

--- VALID_FORM/script.js
+++ VALID_FORM/script.js
@@ -39,10 +39,8 @@ function addForm(teg, formСontent){
                     'check':'checkbox', 
                     'memo':'textarea',
                     'submit':'submit'};
-    var inputElement = [];  
-    var labelElement = [];  
-    var selectElement = [];  
-    var labelForElement = [];
+
+    var inputElement = [], labelElement = [], selectElement = [], labelForElement = [];  
     var divElement = document.createElement("div");
     for(var i = 0; i < formСontent.length; i++)
         {
@@ -81,18 +79,21 @@ function addForm(teg, formСontent){
                 var itemVar=formСontent[i].variants;
                 for(j=0; j<itemVar.length; j++)
                 {
+                   // labelForElement[j] = document.createElement("label");              // variant 1
+                   // labelForElement[j].innerText = itemVar[j].text;                   // vlojit input v labal
+                   // labelElement[i].appendChild(labelForElement[j]);                     
+                    selectElement[j] = document.createElement("input");
+                    selectElement[j].type = objKinds[formСontent[i].kind];                    
+                    selectElement[j].name = formСontent[i].name;
+                    selectElement[j].value = itemVar[j].value;
+                    selectElement[j].id = j;  
+                    selectElement[j].innerText = itemVar[j].text;
+                //    labelForElement[j].appendChild(selectElement[j]);                 //for variant 1
+                    labelElement[i].appendChild(selectElement[j]);
                     labelForElement[j] = document.createElement("label");              
                     labelForElement[j].innerText = itemVar[j].text;  
-                    labelElement[i].appendChild(labelForElement[j]);                     
-                    selectElement[j] = document.createElement("input");
-                    selectElement[j].name = formСontent[i].name;
-                    selectElement[j].type = objKinds[formСontent[i].kind];
-                    selectElement[j].value = itemVar[j].value;
-                //    selectElement[j].id = j;
-                    selectElement[j].innerText = itemVar[j].text;
-                    labelForElement[j].appendChild(selectElement[j]); 
-       
-                //    labelForElement[j].for = selectElement[j].id;
+                    labelForElement[j].for = selectElement[j].id;           //.for ne zadaetsya  
+                    labelElement[i].appendChild(labelForElement[j]);                   
                 }
                 divElement.appendChild(labelElement[i]);  
             }  
@@ -103,41 +104,14 @@ function addForm(teg, formСontent){
             inputElement[i].name = formСontent[i].name;
             if(formСontent[i].caption)
                 inputElement[i].value = formСontent[i].caption;
+            if(formСontent[i].kind === 'check')
+                inputElement[i].checked = 'checked';
             labelElement[i].appendChild(inputElement[i]);
             divElement.appendChild(labelElement[i]);              
         }}
-     //   $input(type='checkbox').atr('checked','checked');
-   /*     inputElement[i].type = objKinds[formСontent[i].kind];
-        
-        objKinds[formСontent[i].kind]
-        
-        if(formСontent[i].name)      
-            inputElement[i].name = formСontent[i].name;
-        
-        if(formСontent[i].caption)
-           inputElement[i].value = formСontent[i].caption;
-        
-        if(formСontent[i].value)
-            inputElement[i].value = formСontent[i].value;
-        if(formСontent[i].text)
-            inputElement[i].innerText = formСontent[i].text;
-    //    console.log(formСontent[i].variants);
-
-        if(formСontent[i].variants)
-            var addItem = addForm(inputElement[i], formСontent[i].variants);
-        labelElement[i].appendChild(inputElement[i]);
-        divElement.appendChild(labelElement[i]);                 
-        }*/
-    //    teg.appendChild(divElement);  
-  //      console.log(divElement);        
 return teg.appendChild(divElement);
 } 
+/*
+function addProp(){
 
-
-/*inputEnement[i].setAttribute('label', formСontent[i].label);
-        inputEnement[i].setAttribute('type', formСontent[i].kind);
-        inputEnement[i].setAttribute('name', formСontent[i].name);
-        inputEnement[i].setAttribute('caption', formСontent[i].caption);
-        inputEnement[i].setAttribute('display', 'block');*/       
-        // labelElement[i].style.padding = '5px';
-       // labelElement[i].style.display = 'block';*/
+}*/
