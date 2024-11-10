83c99b0e83833ca569b7d1a3bbb2a1750a36989b
Vera Sotniokva <orchid86@yandex.ru>
2024-11-10 21:20:48 +0300

    start

--- b6_v2/stroka.js
+++ b6_v2/stroka.js
@@ -1,25 +1,138 @@
 
 //var obj = { teg:'', atr:'' };
-
+/*
 var wrapP = buildWrapper('P');
-var wrapH1 = buildWrapper('H1');
+var wrapH11 = buildWrapper('H1');
 
 
  console.log(wrapP);
  // console.log(wrapH1);  
- var wrapP1 = wrapP("Однажды в студёную зимнюю пору");
-  console.log( wrapP1);
+ //var wrapP1 = wrapP("Однажды в студёную зимнюю пору");
+  //console.log( wrapP1);
+  var a = wrapP("Однажды в студёную зимнюю пору");
+  console.log( wrapP("Однажды в студёную зимнюю пору"));
+  console.log( wrapP("Однажды в студёную зимнюю пору",{lang:"ru"}) );
+  console.log( wrapP("Однажды в <студёную> зимнюю пору") );
+  console.log( wrapH1("СТИХИ",{align:"center",title:"M&M's"}) );
+*/
+
+function buildWrapper(teg)
+{ 
+   var partI = '<' + teg + ' ';
+   var partII = '</' + teg + '>';
+  // var wrap = wrapP();
+ 
+  // var wrap = wrapP(this);
+   //return partI + wrap + partII;
+  // wrapP(text,options) => {
+  /* var aaa = function wrapP(text,options) {
+   options = options || {};
+   var masMnem = {"<":'&#60 ;', ">":'&#62;', "'":'&#39;', '"':'&#34;', "&":'&#38;', ")":'&#41;', ":":'&#58;'};    
+   var myStroka = text; 
+   var myIndex = text.length || 0; 
+   //console.log(myIndex);
+   atr = 0;
+   if(myStroka.indexOf("{") >= 0)
+      {
+      myIndex = myStroka.indexOf("{");
+      atr = myStroka.substring(myIndex + 1, myStroka.length - 1);
+      atr = atr.replace(':', '=');
+      atr = atr.replace(',', ';');
+      }
+   var newStroka = text.substring(0, myIndex - 1);
+   var x, y; 
+   for(var i = 0; i<newStroka.length; i++)  
+      {
+      x = newStroka[i];
+      if(newStroka[i] in masMnem)
+         x = masMnem[newStroka[i]]; 
+      y = y + x;
+      }      
+   let fullStr = atr +">" + y;
+   return fullStr;
+   }*/
+
+   return partI;
+}  
+
+function wrapP(text, options) 
+   {
+   options = options || {};
+   var masMnem = {"<":'&#60 ;', ">":'&#62;', "'":'&#39;', '"':'&#34;', "&":'&#38;', ")":'&#41;', ":":'&#58;'};    
+   var myStroka = text; 
+   var myIndex = text.length; 
+   //console.log(myIndex);
+   atr = 0;
+   if(myStroka.indexOf("{") >= 0)
+      {
+      myIndex = myStroka.indexOf("{");
+      atr = myStroka.substring(myIndex + 1, myStroka.length - 1);
+      atr = atr.replace(':', '=');
+      atr = atr.replace(',', ';');
+      }
+   var newStroka = text.substring(0, myIndex - 1);
+   var x, y; 
+   for(var i = 0; i<newStroka.length; i++)  
+      {
+      x = newStroka[i];
+      if(newStroka[i] in masMnem)
+         x = masMnem[newStroka[i]]; 
+      y = y + x;
+      }      
+   let fullStr = atr +">" + y;
+   return buildWrapper() + fullStr + buildWrapper();
+   }
+
+var wrapP = buildWrapper('P');
+var wrapH11 = buildWrapper('H1');
+
+
+ //console.log(wrapP);
+ // console.log(wrapH1);  
+ //var wrapP1 = wrapP("Однажды в студёную зимнюю пору");
+  //console.log( wrapP1);
+  var a = wrapP("Однажды в студёную зимнюю пору");
+  console.log( wrapP("Однажды в студёную зимнюю пору"));
   console.log( wrapP("Однажды в студёную зимнюю пору",{lang:"ru"}) );
   console.log( wrapP("Однажды в <студёную> зимнюю пору") );
   console.log( wrapH1("СТИХИ",{align:"center",title:"M&M's"}) );
 
 
-function buildWrapper(teg)
-{
-   
-   myText = '<' + teg + ' ' + '>';
-   return myText; 
-}  
+/*
+function wrapP(text,options) 
+   {
+   options = options || {};
+   var masMnem = {"<":'&#60 ;', ">":'&#62;', "'":'&#39;', '"':'&#34;', "&":'&#38;', ")":'&#41;', ":":'&#58;'};    
+   var myStroka = text; 
+   var myIndex = text.length; 
+   //console.log(myIndex);
+   atr = 0;
+   if(myStroka.indexOf("{") >= 0)
+      {
+      myIndex = myStroka.indexOf("{");
+      atr = myStroka.substring(myIndex + 1, myStroka.length - 1);
+      atr = atr.replace(':', '=');
+      atr = atr.replace(',', ';');
+      }
+   var newStroka = text.substring(0, myIndex - 1);
+   var x, y; 
+   for(var i = 0; i<newStroka.length; i++)  
+      {
+      x = newStroka[i];
+      if(newStroka[i] in masMnem)
+         x = masMnem[newStroka[i]]; 
+      y = y + x;
+      }      
+   let fullStr = atr +">" + y;
+   return buildWrapper() + fullStr + buildWrapper();
+   }
+
+*/
+
+
+
+
+
 //function buildWrapper(teg, myStroka, myatr)
 /*
 function buildWrapper(teg)
