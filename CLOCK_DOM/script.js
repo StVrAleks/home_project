31be07f54deb0a534c66461a47b51b8c5ddfcfcf
VeraS. <orchid86@yandex.ru>
2024-11-21 16:48:08 +0300

    v.2

--- CLOCK_DOM/script.js
+++ CLOCK_DOM/script.js
@@ -1,4 +1,4 @@
-setInterval(updateTime,1000);
+//setInterval(updateTime,1000);
 
 window.addEventListener('load', document.getElementsByTagName('input')[0].focus(), false); 
 function validInp(){
@@ -24,6 +24,10 @@ function addClock(){
     
     var clockItems = [];
     var clockSpan = [];
+
+    //100% экрана часов разделим условно на 7 частей (9; 8,10; 7,11; 6,12; 5,1; 4,2; 3), 
+    //получаем, что максимальный по размеру диаметр круга с цифрами может быть 14% от объема большего диаметра,
+    //что и запишем в clockNumber
     var clockNumber = 14;
     for(var i = 0; i<12; i++)
     {
@@ -40,54 +44,63 @@ function addClock(){
       clockItems[i].style.backgroundColor = '#48B382';
 
       clockItems[i].style.fontSize = '20px';
-      clockItems[i].style.left = 100*positionX(i+1, clockNumber) + '%';//Number(diametr) * positionX(i+1, clockNumber) + 'px';
-      clockItems[i].style.top = 100*positionY(i+1, clockNumber) + '%';//Number(diametr) * positionY(i+1, clockNumber)  + 'px';
+      clockItems[i].style.left = 100*positionX(i+1, clockNumber) + '%';
+      clockItems[i].style.top = 100*positionY(i+1, clockNumber) + '%';
       clockItems[i].appendChild(clockSpan[i]);
       clock.appendChild(clockItems[i]);   
     }
     for(var i = 0; i<12; i++)
        clockItems[i].style.position = 'absolute';
        
+    setInterval(updateTime,1000);
 
-    var hour = document.createElement("div"); 
-    hour.id = 'clockHour';
-    hour.style.width = 0.35 * Number(diametr)  + 'px';
-    hour.style.height = '10px';   
-    hour.style.backgroundColor = 'black';
-    hour.style.position = 'absolute'; 
-    hour.style.left = '50%';
-    hour.style.top = '50%'; 
-    hour.style.transform = 'translate(-50%, -50%)';
-    hour.style.transformOrigin = 'top left'; 
-    hour.style.transform = 'rotate(-90deg)';   
-    clock.appendChild(hour);  
+    const currTime = new Date();
+    const hours = currTime.getHours();
+    const minutes = currTime.getMinutes();
+    const seconds = currTime.getSeconds();
+    
+    
+
 
     var min  = document.createElement("div"); 
     min.id = 'clockMin';
-    min.style.width = 0.4 * Number(diametr)  + 'px';
-    min.style.height = '7px';
+    min.style.width = 0.45 * Number(diametr)  + 'px'; //40% на основную стрелку +  5% на "хвостик"
+    min.style.height = '5px';
     min.style.backgroundColor = 'black';
     min.style.position = 'absolute'; 
-    min.style.left = '50%';
+    min.style.left = '45%';
     min.style.top = '50%';
     min.style.transform = 'translate(-50%, -50%)';
-    min.style.transformOrigin = 'top left'; 
-    min.style.transform = 'rotate(-90deg)';   
+    min.style.transformOrigin = '10% 10%'; 
+    min.style.opacity = '0'; 
 
-    clock.appendChild(min);  
+
     var sec  = document.createElement("div"); 
     sec.id = 'clockSec';
-    sec.style.width = 0.5 * Number(diametr)  + 'px';
+    sec.style.width = 0.55 * Number(diametr)  + 'px'; //50% на основную стрелку +  5% на "хвостик"
     sec.style.height = '3px';
-    sec.style.backgroundColor = 'black';
     sec.style.position = 'absolute'; 
-    sec.style.left = '50%';
+    sec.style.left = '45%';
     sec.style.top = '50%';
     sec.style.transform = 'translate(-50%, -50%)';
-    console.log(Number(diametr) / 2);
-    sec.style.transformOrigin = 'top left';
-    sec.style.transform = 'rotate(-90deg)';
-    clock.appendChild(sec); 
+    sec.style.transformOrigin = '10% 10%';
+    sec.style.backgroundColor = 'black';   
+    sec.style.opacity = '0'; 
+
+
+    var hour = document.createElement("div"); 
+    hour.id = 'clockHour';
+    hour.style.position = 'absolute'; 
+    hour.style.width = 0.3 * Number(diametr)  + 'px';      //25% на основную стрелку +  5% на "хвостик"
+    hour.style.left = '45%';
+    hour.style.top = '45%'; 
+    hour.style.transform = 'translate(-50%, -50%)';
+    hour.style.transformOrigin = '10% 10%'; 
+
+    hour.style.height = '10px';   
+    hour.style.backgroundColor = 'black';     
+    hour.style.opacity = '0';     
+   
     
  
     //const currTime=new Date();
@@ -102,28 +115,30 @@ function addClock(){
     timeNew.style.transform = 'translate(-50%, -50%)';
     clock.appendChild(timeNew);     
 
+ clock.appendChild(min);   
+ clock.appendChild(sec); 
+ clock.appendChild(hour);  
+
+
+
+return true;
 }
 
 
 function positionX(num, partClock){
- //partClock = 15
-
+ //partClock = 14%
+//всего 7 частей, отсюда выводим коэффициент для расоложения каждого из кругов и смещаем туда центр "пациента"
  if( num === 1 || num === 5)
- // return 8/12 - partClock*0.5/100; 
-   return 5/7 - partClock*0.5/100; 
+    return 5/7 - partClock*0.5/100; 
  if(num === 2 || num === 4)
-  //return 10/12 - partClock*0.5/100; 
-  return 6/7 - partClock*0.5/100; 
+    return 6/7 - partClock*0.5/100; 
  if(num === 7 || num === 11)
-  //return  4/12 - partClock*0.5/100; 
-  return  2/7 -  partClock*0.5/100; 
+    return  2/7 -  partClock*0.5/100; 
  if(num === 8 || num === 10)
-  //return  2/12 - partClock*0.5/100; 
-  return  1/7 - partClock*0.5/100; 
+    return  1/7 - partClock*0.5/100; 
 
  if(num === 6 || num === 12)
-  //return  6/12 - partClock*0.5/100; 
-  return  0.5 - 1/14; 
+    return  4/7 - partClock/100; 
  if(num === 3)
    return 6/7;
  if(num === 9)
@@ -131,27 +146,23 @@ function positionX(num, partClock){
 }
 
 function positionY(num, partClock){
-    if(num === 1 || num === 11)
-     // return  2/12 - 0.5*partClock/100;  
-     return  1/7 - partClock*0.5/100;//partClock/100;  
+    if(num === 1 || num === 11)  
+      return  1/7 - partClock*0.5/100;
     if(num === 2 || num === 10)
-      //return  4/12 - partClock*0.5/100; 
-    return  2/7 - partClock*0.5/100; 
+      return  2/7 - partClock*0.5/100; 
 
     if(num === 4 || num === 8)
-      //return 8/12 - partClock*0.5/100;  
       return 4/7 + partClock*0.5/100;    
     if(num === 5 || num === 7)
-      //return 10/12 - partClock*0.5/100; 
       return 5/7 + partClock*0.5/100; 
 
     if(num === 6)
-       return 1 - partClock/100;
+       return 5/7 + partClock/100;
     if(num === 12)
-       return 0.5/100;
+       return 1/7 - partClock/100;
 
     if(num === 3 || num === 9)
-      return  6/12 - partClock*0.5/100;       
+      return  4/7 - partClock/100;       
    }
 
    function updateTime() {
@@ -170,19 +181,28 @@ function positionY(num, partClock){
       const ygolSec = -90 + seconds * 6;
       const ygolMin = -90 + minutes * 6;
       const ygolHours = -90 + (hours-12) * 30 + 5/60*minutes*6;
-      console.log(-90 + (hours-12)*30 , 5/60*minutes*6);
+   
+      //  console.log(-90 + (hours-12)*30 , 5/60*minutes*6);
       if(document.getElementById('clockSec'))
         document.getElementById('clockSec').style.transform = 'rotate(' + ygolSec + 'deg)';
       if(document.getElementById('clockMin'))
         document.getElementById('clockMin').style.transform = 'rotate(' + ygolMin  + 'deg)';
       if(document.getElementById('clockHour'))
         document.getElementById('clockHour').style.transform = 'rotate(' + ygolHours + 'deg)';
+      
+      
+      document.getElementById('clockSec').style.opacity = '1';
+      document.getElementById('clockMin').style.opacity = '1';
+      document.getElementById('clockHour').style.opacity = '1';   
+
+
       return str0l(hours,2) + ':' + str0l(minutes,2) + ':' + str0l(seconds,2);
   }
+
    // дополняет строку val слева нулями до длины Len
    function str0l(val,len) {
       let strVal=val.toString();
-      while ( strVal.length < len )
+      while (strVal.length < len)
           strVal='0'+strVal;
       return strVal;
   }
