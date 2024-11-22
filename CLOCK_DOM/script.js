7d6a01bd33fc7b462e2cb7808e8b367a9467ed10
VeraS. <orchid86@yandex.ru>
2024-11-22 18:32:11 +0300

    v2

--- CLOCK_DOM/script.js
+++ CLOCK_DOM/script.js
@@ -13,13 +13,10 @@ function addClock(){
     document.getElementsByTagName('button')[0].style.display = 'none';  
 
     var clock = document.createElement("div"); 
-    clock.id = 'bigClock';
+
     clock.style.width = Number(diametr) + 'px';
     clock.style.height = Number(diametr) + 'px';
-    clock.style.borderRadius = '50%';
-  //  clock.style.padding = '2%';
-    clock.style.backgroundColor = '#FCCA66';
-    clock.style.position = 'absolute';
+    clock.id = 'bigClock';   
     document.body.appendChild(clock); 
     
     var clockItems = [];
@@ -28,7 +25,7 @@ function addClock(){
     //100% экрана часов разделим условно на 7 частей (9; 8,10; 7,11; 6,12; 5,1; 4,2; 3), 
     //получаем, что максимальный по размеру диаметр круга с цифрами может быть 14% от объема большего диаметра,
     //что и запишем в clockNumber
-    var clockNumber = 14;
+    var clockNumber = 100/7;
     for(var i = 0; i<12; i++)
     {
       clockItems[i] = document.createElement("div"); 
@@ -54,53 +51,22 @@ function addClock(){
        
     setInterval(updateTime,1000);
 
-    const currTime = new Date();
-    const hours = currTime.getHours();
-    const minutes = currTime.getMinutes();
-    const seconds = currTime.getSeconds();
-    
-    
-
-
     var min  = document.createElement("div"); 
     min.id = 'clockMin';
-    min.style.width = 0.45 * Number(diametr)  + 'px'; //40% на основную стрелку +  5% на "хвостик"
-    min.style.height = '5px';
-    min.style.backgroundColor = 'black';
-    min.style.position = 'absolute'; 
-    min.style.left = '45%';
-    min.style.top = '50%';
-    min.style.transform = 'translate(-50%, -50%)';
-    min.style.transformOrigin = '10% 10%'; 
-    min.style.opacity = '0'; 
+    min.style.height = 0.40 * Number(diametr)  + 'px'; 
 
 
     var sec  = document.createElement("div"); 
     sec.id = 'clockSec';
-    sec.style.width = 0.55 * Number(diametr)  + 'px'; //50% на основную стрелку +  5% на "хвостик"
-    sec.style.height = '3px';
-    sec.style.position = 'absolute'; 
-    sec.style.left = '45%';
-    sec.style.top = '50%';
-    sec.style.transform = 'translate(-50%, -50%)';
-    sec.style.transformOrigin = '10% 10%';
-    sec.style.backgroundColor = 'black';   
-    sec.style.opacity = '0'; 
+    sec.style.height = 0.5 * Number(diametr)  + 'px';
 
 
     var hour = document.createElement("div"); 
     hour.id = 'clockHour';
-    hour.style.position = 'absolute'; 
-    hour.style.width = 0.3 * Number(diametr)  + 'px';      //25% на основную стрелку +  5% на "хвостик"
-    hour.style.left = '45%';
-    hour.style.top = '45%'; 
-    hour.style.transform = 'translate(-50%, -50%)';
-    hour.style.transformOrigin = '10% 10%'; 
-
-    hour.style.height = '10px';   
-    hour.style.backgroundColor = 'black';     
-    hour.style.opacity = '0';     
+    hour.style.height = 0.25 * Number(diametr) + 'px';
    
+    const currTime=new Date();
+    var gegRot = formatDateTime(currTime);
     
  
     //const currTime=new Date();
@@ -175,12 +141,14 @@ function positionY(num, partClock){
 
   // форматирует дату-время в формате чч:мм:сс
   function formatDateTime(dt) {
+      const degSec = 360/60;
+      const degSectionHour = 5/60;
       const hours=dt.getHours();
       const minutes=dt.getMinutes();
       const seconds=dt.getSeconds();
-      const ygolSec = -90 + seconds * 6;
-      const ygolMin = -90 + minutes * 6;
-      const ygolHours = -90 + (hours-12) * 30 + 5/60*minutes*6;
+      const ygolSec = 180 + seconds * degSec;
+      const ygolMin = 180 + minutes * degSec;
+      const ygolHours = 180 + (hours-12) * 30 + degSectionHour*minutes*degSec;
    
       //  console.log(-90 + (hours-12)*30 , 5/60*minutes*6);
       if(document.getElementById('clockSec'))
@@ -190,10 +158,6 @@ function positionY(num, partClock){
       if(document.getElementById('clockHour'))
         document.getElementById('clockHour').style.transform = 'rotate(' + ygolHours + 'deg)';
       
-      
-      document.getElementById('clockSec').style.opacity = '1';
-      document.getElementById('clockMin').style.opacity = '1';
-      document.getElementById('clockHour').style.opacity = '1';   
 
 
       return str0l(hours,2) + ':' + str0l(minutes,2) + ':' + str0l(seconds,2);
