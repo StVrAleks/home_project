9db69d9a7d9cd0db5ef6094201ee87af43d4e6a5
Vera Sotniokva <orchid86@yandex.ru>
2024-11-30 11:30:08 +0300

    v.2

--- CLOCK_CANVAS/script.js
+++ CLOCK_CANVAS/script.js
@@ -1,4 +1,4 @@
-//setInterval(updateTime,1000);
+//setInterval(picClock,1000);
 
 window.addEventListener('load', document.getElementsByTagName('input')[0].focus(), false); 
 function validInp(){
@@ -11,19 +11,20 @@ function addClock(){
     var diametr = document.getElementsByTagName('input')[0].value;
     document.getElementsByTagName('input')[0].style.display = 'none';
     document.getElementsByTagName('button')[0].style.display = 'none';  
+    updateTime();
    
-    setInterval(picClock(diametr),1000);
 }
-function picClock(diametr){
+function picClock(){
 
     const xmlns = "http://www.w3.org/2000/svg";  
-  //  var diametr = document.getElementsByTagName('input')[0].value;
+    var diametr = document.getElementsByTagName('input')[0].value;
       var elClock = document.getElementById('bigClock');
         
       const contextClock = elClock.getContext("2d");
       contextClock.canvas.width = diametr;
       contextClock.canvas.height = diametr;
-      var radius =  contextClock.canvas.width / 2; 
+      var radius =  contextClock.canvas.width / 2;
+      
       contextClock.clearRect(0, 0, contextClock.canvas.width, contextClock.canvas.height);
       contextClock.beginPath();
 
@@ -41,7 +42,6 @@ function picClock(diametr){
           contextClock.fillStyle = "red";          
           contextClock.moveTo(ygolCkockX, ygolCkockY);
           contextClock.beginPath();
-          //contextClock.fillStyle = "red";
           contextClock.arc(
                 ygolCkockX,
                 ygolCkockY,
@@ -52,105 +52,117 @@ function picClock(diametr){
           contextClock.moveTo(ygolCkockX, ygolCkockY);
           contextClock.fillStyle = "black"; 
           contextClock.textAlign = 'center';
-        //  contextClock.fontSize = '50px';
           contextClock.font = diametr*0.07 +'px Arial';
           contextClock.textBaseline = 'middle';
           contextClock.fillText(i + 1, ygolCkockX, ygolCkockY);
           contextClock.fill();
       }
+      //time
+      const currTime = new Date();
+      const degSec = 360/12;
+      const hours=currTime.getHours();
+      const minutes=currTime.getMinutes();
+      const seconds=currTime.getSeconds();
+      const ygolSec = seconds * 360/60;
+      const ygolMin = minutes * 360/60;
+      const ygolHours = (hours+minutes/60) * degSec;
+
       //sec
-      contextClock.moveTo(radius, radius);
-     // contextClock.rotate((45 * Math.PI) / 180);      
+      ygolCkockX = diametr*0.5 + radius * Math.sin(360/12/180*Math.PI*(seconds/5))*0.85;
+      ygolCkockY = diametr*0.5 - radius * Math.cos(360/12/180*Math.PI*(seconds/5))*0.85;
+      contextClock.moveTo(radius, radius);   
+      contextClock.beginPath();  
       var lines = [
         {x: radius, y: radius},
-        {x: radius - 5, y: radius - radius /2 },
-        {x: radius, y: 5},
-        {x: radius + 5, y: radius - radius /2},
+       // {x: radius - 5, y: radius - radius /2 },
+        {x: ygolCkockX, y: ygolCkockY},
+      //  {x: radius + 5, y: radius - radius /2},
         {x: radius, y: radius},
       ],
       line = lines.shift();
+      contextClock.lineWidth = radius*0.01;
       do {
         contextClock.moveTo(line.x, line.y);
         line = lines.shift();
-        contextClock.lineTo(line.x, line.y);
-        
+        contextClock.lineTo(line.x, line.y);      
       } while (lines.length);
-        //contextClock.translate((45 * Math.PI) / 180 );
-
       contextClock.stroke();
+      contextClock.moveTo(ygolCkockX, ygolCkockY); 
+      contextClock.arc(ygolCkockX, ygolCkockY, radius*0.0015, 0, 2 * Math.PI);
+      contextClock.fill();
 
-       //min
+//********************* */
+      //min
+      ygolCkockX = diametr*0.5 + radius * Math.sin(360/12/180*Math.PI*(minutes/5))*0.75;
+      ygolCkockY = diametr*0.5 - radius * Math.cos(360/12/180*Math.PI*(minutes/5))*0.75;
+ 
        contextClock.moveTo(radius, radius);
+       contextClock.beginPath();
        var lines = [
          {x: radius, y: radius},
-         {x: radius - 10, y: radius - radius /3},
-         {x: radius, y: radius / 3},
-         {x: radius + 10, y: radius - radius /3},
+       //  {x: radius - 10, y: radius - radius /3},
+         {x: ygolCkockX, y: ygolCkockY},
+         {x: ygolCkockX, y: ygolCkockY},
+       //  {x: radius + 10, y: radius - radius /3},
          {x: radius, y: radius},
        ],
        line = lines.shift();
+       contextClock.lineWidth = radius*0.015;
        do {
          contextClock.moveTo(line.x, line.y);
          line = lines.shift();
          contextClock.lineTo(line.x, line.y);
        } while (lines.length);
- 
-       contextClock.stroke();
+       contextClock.stroke();       
+       contextClock.moveTo(ygolCkockX, ygolCkockY); 
+       contextClock.arc(ygolCkockX, ygolCkockY, radius*0.005, 0, 2 * Math.PI);
+       contextClock.fill();
 
-       //hour
+
+//************* 
+        //hour    
+       ygolCkockX = diametr*0.5 + radius * Math.sin(360/12/180*Math.PI*(hours + minutes/60))*0.65;
+       ygolCkockY = diametr*0.5 - radius * Math.cos(360/12/180*Math.PI*(hours + minutes/60))*0.65;
+  
        contextClock.moveTo(radius, radius);
+       contextClock.beginPath();
        var lines = [
          {x: radius, y: radius},
-         {x: radius - 20, y: radius - radius / 4},
-         {x: radius, y: radius / 4},
-         {x: radius + 20, y: radius - radius / 4},
+       //  {x: radius - 20, y: radius - radius / 4},
+         {x: ygolCkockX, y: ygolCkockY},
+       //  {x: radius + 20, y: radius - radius / 4},
          {x: radius, y: radius},
        ],
        line = lines.shift();
+       
        do {
          contextClock.moveTo(line.x, line.y);
          line = lines.shift();
          contextClock.lineTo(line.x, line.y);
+         contextClock.lineWidth = radius*0.02;
        } while (lines.length);
- 
        contextClock.stroke();
-
-      
-    
-   var gegRot = updateTime(); 
+       contextClock.moveTo(ygolCkockX, ygolCkockY); 
+       contextClock.arc(ygolCkockX, ygolCkockY, radius*0.007, 0, 2 * Math.PI);      
+       contextClock.fill();
+//*********** */
+//timer
+       contextClock.moveTo(radius*0.70, radius*0.85);
+       contextClock.fillStyle = "black"; 
+       contextClock.textAlign = 'center';
+       contextClock.font = diametr*0.07 +'px Arial';
+       contextClock.textBaseline = 'middle';
+       contextClock.fillText(str0l(hours,2) + ':' + str0l(minutes,2) + ':' + str0l(seconds,2), diametr*0.5, diametr*0.25);
+       contextClock.fill();  
 
 return true;
 }
 
-
    function updateTime() {
       const currTime=new Date();
-      var currTimeStr = formatDateTime(currTime);
-      if(document.getElementById('curD'))
-         document.getElementById('curD').textContent = currTimeStr;
-      console.log(currTimeStr);  
+      setInterval(picClock,1000); 
+      return currTime;
    }
-
-  // форматирует дату-время в формате чч:мм:сс
-  function formatDateTime(dt) {
-      const degSec = 360/12;
-      const hours=dt.getHours();
-      const minutes=dt.getMinutes();
-      const seconds=dt.getSeconds();
-      const ygolSec = seconds * 360/60;
-      const ygolMin = minutes * 360/60;
-      const ygolHours = (hours+minutes/60) * degSec;
-   
-      
-   /*   if(document.getElementById('clockSec'))
-        document.getElementById('clockSec').style.transform = 'rotate(' + ygolSec + 'deg)';
-      if(document.getElementById('clockMin'))
-        document.getElementById('clockMin').style.transform = 'rotate(' + ygolMin  + 'deg)';
-      if(document.getElementById('clockHour'))
-        document.getElementById('clockHour').style.transform = 'rotate(' + ygolHours + 'deg)';*/
-      return str0l(hours,2) + ':' + str0l(minutes,2) + ':' + str0l(seconds,2);
-  }
-
    // дополняет строку val слева нулями до длины Len
    function str0l(val,len) {
       let strVal=val.toString();
