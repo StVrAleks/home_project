d34c4c343cfc2c5e5c49f21bb468efcf684cc047
Vera Sotniokva <orchid86@yandex.ru>
2024-12-18 22:07:52 +0300

    3

--- itogovyi/script01.js
+++ itogovyi/script01.js
@@ -1,35 +1,72 @@
 const xmlns = "http://www.w3.org/2000/svg"; 
 
-var canvas = document.getElementById('logo');
+var allPartGame = document.getElementById('place_game_out');
+console.log(allPartGame);
+var allPart = allPartGame.getBoundingClientRect();
+var allPartWidth = allPart.width;
+var allPartHeight = allPart.height;
+var allPartY = (allPartHeight - allPartGame.offsetTop)/2;
+var allPartX = (allPartWidth - allPartGame.offsetLeft)/4;
+var posY = allPartX - allPartHeight/2;
+
+var itemLogo = document.getElementById('logo');
+var partLogo = itemLogo.getBoundingClientRect();
+var logoWidth = partLogo.width;
+var logoHeight = partLogo.height;
+var logoY = itemLogo.offsetTop;
+var logoX = itemLogo.offsetLeft;
+
+var canvas = document.getElementById('game_canvas');
+console.log(logoWidth, logoHeight, logoX, logoY);
+canvas.style.width = allPartWidth + 'px';
+canvas.style.height = allPartHeight + 'px';
+canvas.style.position = 'absolute';
+canvas.style.top = 50 + '%';//'112px';//
+canvas.style.left = 50 + '%';//'281px';//
+canvas.style.transform = 'translate(-50%,-25%)';
+canvas.style.zIndex = '10';
+
+const ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.lineWidth = 1;
+ctx.fillStyle = "green"; 
+logoX=canvas.offsetLeft;
+console.log(canvas.offsetLeft);
+ctx.moveTo(logoX, logoY);
+ctx.lineTo(logoX*0.48, logoY*0.66);
+ctx.lineTo(logoX*0.4, logoY*0.9);
+ctx.lineTo(logoX*0.6, logoY*0.66);
+ctx.lineTo(logoX*0.51, logoY*0.9);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 1;
+ctx.fillStyle = "black"; 
+ctx.moveTo(logoX, logoY*0.9);
+ctx.moveTo(logoX*0.5, logoY*1.2);
+ctx.lineTo(logoX*0.8, logoY*0.8);
+/*ctx.lineTo(logoX + 60, logoY  - 10);
+ctx.lineTo(logoX + 180, logoY + 45);
+ctx.lineTo(logoX + 140, logoY  - 2);*/
+/*ctx.stroke();
+ctx.closePath();*/
+
+
+
+
+/*var canvas = document.getElementById('logo');
 var logoPos = canvas.getBoundingClientRect();
 var logoW = logoPos.width;
 var logoH = logoPos.height;
 var logoX = Math.round(logoPos.top) - logoW;
 var logoY = Math.round(logoPos.left) - logoH;
-console.log(logoW, logoX, logoY);       
-const ctx = canvas.getContext("2d");
-
-ctx.beginPath();
-ctx.lineWidth = 10;
-ctx.fillStyle = "black"; 
-ctx.moveTo(logoX-50, logoY + 15);
-ctx.lineTo(logoX, logoY + 15);
-ctx.lineTo(logoX-50, logoY + 70);
-ctx.lineTo(logoX+80, logoY + 15);
-ctx.lineTo(logoX+20, logoY + 80);
-//logos.shift(); 
-ctx.stroke();
-ctx.beginPath();
-ctx.moveTo(logoX - 100, logoY + logoH*2 - 2);
-ctx.moveTo(logoX - 40, logoY + logoH*2 - 2);
-ctx.lineTo(logoX + 110, logoY + 45);
-ctx.lineTo(logoX + 60, logoY + logoH*2 - 10);
-ctx.lineTo(logoX + 180, logoY + 45);
-ctx.lineTo(logoX + 140, logoY + logoH*2 - 2);
-ctx.closePath();
-ctx.stroke();
+console.log(logoW, logoX, logoY);     */  
+//const ctx = canvas.getContext("2d");
 
 
+
+/*
 var gamePart = document.getElementById('for_game_layer4');
 var gameX = gamePart.offsetTop;
 var gameY = gamePart.offsetLeft;
@@ -66,6 +103,7 @@ svg.style.left = svgY + 'px';
   //var home = document.createElementNS(svg.namespaceURI, 'line');  
 //  home = svg.getContext("2d");
 //var home = svg.getContext("2d");
+/*
 var home = document.createElementNS(svg.namespaceURI, 'line');
     home.setAttribute('stroke', 'grey');
     home.setAttribute('stroke', 'grey');
